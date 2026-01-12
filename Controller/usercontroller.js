d3f3f6b8a1dc08e6baf0787bb4f880bae99ec916
kelicodes <kelidevske@gmail.com>
2026-01-12 12:56:51 +0300

    u

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -112,3 +112,18 @@ export const logout = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+export const me = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId).select("-password"); // exclude password
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-import { login,logout,signup } from "../Controller/usercontroller.js";
+import { login,logout,signup,me } from "../Controller/usercontroller.js";
 import express from "express"
 
 
@@ -7,6 +7,7 @@ const userRouter=express.Router()
 userRouter.post("/signup",signup)
 userRouter.post("/login",login)
 userRouter.post("/",logout)
+userRouter.get("/me",me)
 
 
 export default userRouter
